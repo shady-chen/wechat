8c2f1128d795afc85fb06baeae024cac632eaac1
shady-chen <710254315@qq.com>
2018-01-25 21:50:06 +0800

    新增网站hello world

--- g.js
+++ g.js
@@ -7,12 +7,9 @@ var xml = require('./answer.js');
 
 
 module.exports = function(opts){
-        //return ganarator function 
-        //
- //       var wechat = new Wechat(opts);
         return function*(next)
         {
-		var that = this;
+		        var that = this;
                 var token = opts.token;
                 var signature = this.query.signature;
                 var nonce = this.query.nonce;
@@ -26,33 +23,30 @@ module.exports = function(opts){
                 {
                       if(sha == signature){this.body = echostr+"";}
                       else{this.body = "wrong"; }
-			console.log("get error");
                 }
                 else if(this.method == "POST")
                 {
                         //处理XML包
                        if(sha != signature){this.body = "wrong";return false;}
                        
-                       var data = yield getRawBody(this.req,{
+                       var data = yield getRawBody(this.req,
+                       {
                         length:this.length,
                         limit:"1mb",
                         encoding:this.charser,
                        });
-		        var content = yield util.parseXMLAsync(data);
-			var message = util.formatMessage(content.xml);
-			var msg = xml.getType(message);
-			console.log(msg);
-			that.type = "application/xml";
-			that.status = 200;
-			that.body =  msg;
-			console.log(message);	
-			return;
+                        var content = yield util.parseXMLAsync(data);
+                        var message = util.formatMessage(content.xml);
+                        var msg = xml.getType(message);
+                        console.log(msg);
+                        that.type = "application/xml";
+                        that.status = 200;
+                        that.body =  msg;
+                        return;
                 }
                 else
                 {
-                        console.log('we found some problems');
-                        return this.body = "wrong!";
-
+                    return this.body = "wrong!";
                 }
 
 
